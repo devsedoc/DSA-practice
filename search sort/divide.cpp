018a4a614e99f46ae03b3df9b5ced3137cfc1b72
devang sharma <devangsharma200623@gmail.com>
2026-09-17 00:57:50 +0530

    modified

--- search sort/divide.cpp
+++ search sort/divide.cpp
@@ -10,15 +10,15 @@ int main(){
 
 int solve(int divisor, int dividend) {
     int s =0;
-    int e = dividend;
+    int e = abs(dividend);
     int mid = s + (e-s)/2;
      while(s<=e){
         //perfect solution
-        if ( mid*divisor == dividend){
+        if ( abs(mid*divisor) == abs(dividend)){
             return mid;
         }
         // not perfect solution
-        else if (mid*divisor > dividend){
+        else if (abs(mid*divisor) > abs(dividend)){
             //left side
             e = mid - 1;
         }
@@ -30,7 +30,12 @@ int solve(int divisor, int dividend) {
             s = mid + 1;
         }
         mid = s + (e-s)/2;
-
+        if((divisor<0 && dividend<0) || (divisor>0 && dividend>0)){
+            return ans;
+        }
+        else{
+            return -ans;
+        }
      }
-     return ans;
+   
     }
